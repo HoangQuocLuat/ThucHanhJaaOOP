58d9adfa0e40145a48bfda0f8f76af60b3919ce3
HoangQuocLuat <2021050769@student.humg.edu.vn>
2022-03-07 17:58:14 +0700

    BaiThucHanh0703

---
+++ BaiThucHanh0703/src/App90.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class App90{
+    public static void main(String[] args) {
+        String chuoi;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhập vào chuỗi bất kỳ: ");
+        chuoi = scanner.nextLine();
+
+        System.out.println("Các ký tự có trong chuỗi là: ");
+
+        for (int i = 0; i < chuoi.length(); i++) {
+        
+        System.out.println(chuoi.charAt(i));
+        }
+        }
+        
+}
