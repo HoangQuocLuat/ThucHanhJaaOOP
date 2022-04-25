621494d89725174572abd424315d0ecdc70986b3
HoangQuocLuat <2021050769@student.humg.edu.vn>
2022-04-25 17:55:09 +0700

    BaiThucHanh2504

---
+++ BaiThucHanh2504/src/Car/TaxiCar.java
@@ -0,0 +1,19 @@
+package Car;
+
+public class TaxiCar implements Car {
+    public void Move() {
+
+    };
+    public void Stop() {
+
+    };
+    public void TurnRight() {
+
+    };
+    public void TurnLeft() {
+
+    };
+    public void Reverse() {
+        
+    };
+}
